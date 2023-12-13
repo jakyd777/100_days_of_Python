3a3f7be1bff87c07a13327610b6da04b3ff6da74
Vladimir Jakubik <137331588+jakyd777@users.noreply.github.com>
2023-12-13 08:53:33 +0100

    Update main.py

--- Day-45-100-movies-scrap/main.py
+++ Day-45-100-movies-scrap/main.py
@@ -8,12 +8,6 @@ response = requests.get(new_url)
 page = response.text
 content = BeautifulSoup(page, "html.parser")
 
-# film = content.find(name="h3", class_="_h3_cuogz_1")
-# film_list = film.getText()
-# print(film_list)
-# # text = "1. 2001: A Space Odyssey (1968)"
-# text_split = text.split("&nbsp;")
-# print(text_split)
 films = content.find_all(name="h3", class_="_h3_cuogz_1")
 
 film_list = [film.getText().split("\xa0")[1] for film in films[:-1]]
